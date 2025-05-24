eb6adcac28abb6cf9b354dffe35bdeac75692bc7
Douso <dousocolombia@gmail.com>
2025-05-23 22:49:41 -0500

    configuración rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('/hola_mundo', 'Home::hola_mundo');
+$routes->get('/zamir', 'Home::zamir');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,7 +11,26 @@ class Home extends BaseController
 
     public function hola_mundo(): string
     {
+
+        return view('repositorio/holamundo');
+        
+    }
+
+    public function nicolas(): string
+    {
         return view('repositorio/holamundo');
     }
+
+    public function zamir(): string
+    {
+        return view('repositorio/zamir/mision_vision');
+    }
+
+    public function santiago(): string
+    {
+        return view('repositorio/holamundo');
+    }
+
+
 
 }
